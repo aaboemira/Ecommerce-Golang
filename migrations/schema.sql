d4864f3387c57c94c7ce435c55d44d563969ee6f
Ahmed Abouemaira <a.aboemira1@gmail.com>
2023-02-11 19:24:00 +0200

    v1.01

--- migrations/schema.sql
+++ migrations/schema.sql
@@ -30,6 +30,7 @@ CREATE TABLE `items` (
   `price` int(11) NOT NULL,
   `created_at` datetime NOT NULL DEFAULT current_timestamp(),
   `updated_at` datetime NOT NULL DEFAULT current_timestamp(),
+  `image` varchar(255) NOT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4;
 /*!40101 SET character_set_client = @saved_cs_client */;
@@ -156,4 +157,4 @@ CREATE TABLE `users` (
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2023-02-10  3:29:42
+-- Dump completed on 2023-02-11 19:22:26
